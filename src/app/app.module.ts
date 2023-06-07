b1b5e48bc513c923db4591d16bac7bc0c3a8d4d9
Enoch Offiah <enochoffiah@yahoo.co.uk>
2023-06-07 23:25:55 +0100

    feat: Success & Warning Alert components
    if applied:
    - Adds two new components - SuccessAlert & WarningAlert
    - Adds two new stylesheet changes for both components
    - Updates declarations array in app.module.ts
    - Updates app.component.ts file with template selectors for both components
    Issue #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
+import { SuccessAlertComponent } from "./success-alert/success-alert.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
